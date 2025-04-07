8ef74f6824acb6e4fdd9320007886759eb025262
Edouard Dieppois <Dieppoisedouard@gmail.com>
2025-04-07 16:43:11 +0200

    Devise : Custom Sign_up with error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def field_error_messages(resource, field)
+    return unless resource.errors[field].any?
+
+    content_tag(:div, class: "text-red-600 text-sm mt-1") do
+      resource.errors[field].map do |msg|
+        content_tag(:div, msg)
+      end.join.html_safe
+    end
+  end
 end

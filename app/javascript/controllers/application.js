e1134c4e592831929925ed9924bade0c65d12334
Edouard Dieppois <Dieppoisedouard@gmail.com>
2025-04-08 17:42:43 +0200

    Swiper import for cards slide + setup arrow in CSS

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,18 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
+
+const swiper = new Swiper(".swiper", {
+	loop: true,
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
